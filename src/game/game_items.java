52f6e38785d9d3cb229cf097e1493849dbbb2bff
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-13 16:13:04 +0500

    Version 1.0

---
+++ src/game/game_items.java
@@ -0,0 +1,16 @@
+package game;
+
+public class game_items {
+	// Game variables
+	String[] enemies = { "Xalufuge", "Tech-Master", "Scorpion", "Spider" };
+	int maxEnemyHealth = 75;
+	int enemyAttackDamage = 25;
+
+	// Player variables
+	int health = 100;
+	int attackDmg = 50;
+	int numHealthPots = 3;
+	int healthPotionHealAmount = 30;
+	int healthPotionDropChance = 50; // Percentage
+
+}
